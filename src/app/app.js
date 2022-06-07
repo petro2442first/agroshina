60a7886260a6ae3046e13fb9a096b06e2bfab8a2
Danil Petrenko <mrb13022001@gmail.com>
2022-06-07 12:45:40 +0300

    init

---
+++ src/app/app.js
@@ -0,0 +1,47 @@
+import "normalize.css";
+import Swiper, { Autoplay, Scrollbar, FreeMode, Thumbs } from "swiper";
+import "swiper/swiper-bundle.min.css";
+
+function initPartnersSlider() {
+  const selector = document.querySelector(".partners-slider") ?? null;
+  console.log(selector);
+  if (selector === null) return;
+
+  const slider = new Swiper(selector, {
+    modules: [Scrollbar, Autoplay],
+    effect: "slide",
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 6,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
+  slider.init();
+}
+
+function initSingleProductThumbsSlider() {
+  var swiper = new Swiper(".thumbs-slider", {
+    modules: [FreeMode],
+    spaceBetween: 30,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".main-thumbs-slider", {
+    modules: [Thumbs],
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}
+
+window.addEventListener("load", (e) => {
+  initPartnersSlider();
+  initSingleProductThumbsSlider();
+});
