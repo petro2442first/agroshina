dc8d0e61df67797fd5012eda22e3f69b45ff0d28
Danil Petrenko <mrb13022001@gmail.com>
2022-06-09 17:18:57 +0300

    add some adaptive layouts

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,16 +2,19 @@ const mix = require("laravel-mix");
 
 mix
   .setPublicPath("./public")
-  .js("src/app/app.js", "public/")
-  .sass("src/style/style.scss", "public/")
+  .js("src/app/app.js", "public/app.js")
+  .sass("src/style/style.scss", "public/style.css")
   .options({
     autoprefixer: "last 5 versions",
     processCssUrls: false,
   })
   .copyDirectory("src/assets/images/", "public/images")
   .copyDirectory("src/assets/fonts/", "public/fonts")
-  .browserSync({
-    watch: true,
-    server: "./public",
-  })
-  .version();
+  .browserSync();
+// .copyDirectory("templates/", "public/")
+// .browserSync({
+//   // proxy: "http://localhost:3000",
+//   watch: true,
+//   server: "./public",
+// });
+// .version();
